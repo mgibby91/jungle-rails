26b1323414489d6611593c760510130545039c8f
Matthew Gibson <mattgibson6@gmail.com>
2020-09-16 20:59:28 +0000

    add veiw and add new category for admin

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,28 @@
+class Admin::CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    @category.save
+
+    redirect_to [:admin, :categories], notice: 'Product created!'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+    )
+  end
+
+
+end
